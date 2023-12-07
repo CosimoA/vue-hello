8efc3ffa6910de8f3ee69904e1f0593e05dd4d69
CosimoA <cosimo.archip@gmail.com>
2023-12-07 19:07:42 +0100

    FEDAT: bonus 1 : add img to HTML DOM with vue js

--- js/script.js
+++ js/script.js
@@ -10,6 +10,8 @@ createApp({
     data(){
         return {
             saluto: "Bitcoin to the Moon!",
+            img: "https://images.unsplash.com/photo-1593720213428-28a5b9e94613?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            descrizioneImmagine: "Terminale Hacker",
         }
     }
-}).mount("#title")
+}).mount("#app")
